18fde0e608882d73acb3cc7252d0ab3a2d7245f9
tony <2252485890@qq.com>
2020-01-30 19:02:21 +0800

    实现广告轮播图，缺陷：多张图片无法展示

--- qingcheng_web_portal/src/main/java/com/qingcheng/controller/IndexController.java
+++ qingcheng_web_portal/src/main/java/com/qingcheng/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.qingcheng.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.qingcheng.pojo.business.Ad;
+import com.qingcheng.service.business.AdService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -9,8 +12,24 @@ import java.util.List;
 @Controller
 public class IndexController {
 
+    /**
+     * 实现首页的跳转
+     * @return
+     */
+
+    @Reference
+    private AdService adService ;
+
     @GetMapping("/index")
-    public String findByPosition(){
+    public String findByPosition(Model model){
+        List<Ad> adList = this.adService.findByPosition("web_index_lb");
+        model.addAttribute("lbt",adList);
         return "index";
+    }
+    @GetMapping("/index2")
+    public String findByPosition2(Model model){
+        List<Ad> adList = this.adService.findByPosition("web_index_lb");
+        model.addAttribute("lbt",adList);
+        return "index2";
     }
 }
